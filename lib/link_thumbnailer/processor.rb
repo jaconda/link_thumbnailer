c91c673d1d2c881dc163c17388be6fc90420a6a0
Anton Mironov <ant.mironov@gmail.com>
2015-03-02 19:55:00 +0300

    request gzipped pages and decompression them

--- lib/link_thumbnailer/processor.rb
+++ lib/link_thumbnailer/processor.rb
@@ -37,7 +37,7 @@ module LinkThumbnailer
 
     def set_http_headers
       http.headers['User-Agent']               = user_agent
-      http.override_headers['Accept-Encoding'] = 'none'
+      http.override_headers['Accept-Encoding'] = 'gzip, deflate'
     end
 
     def set_http_options
@@ -49,11 +49,29 @@ module LinkThumbnailer
     def perform_request
       response = http.request(url)
       case response
-      when ::Net::HTTPSuccess then response.body
+      when ::Net::HTTPSuccess then decode(response['content-encoding'], response.body)
       when ::Net::HTTPRedirection
         call resolve_relative_url(response['location']), redirect_count + 1
       else
         response.error!
+      end
+    end
+
+    def decode(content_encoding, body)
+      if (!body) || body.empty?
+        body
+      elsif content_encoding == 'gzip'
+        Zlib::GzipReader.new(StringIO.new(body)).read
+      elsif content_encoding == 'deflate'
+        begin
+          Zlib::Inflate.new.inflate body
+        rescue Zlib::DataError
+          # No luck with Zlib decompression. Let's try with raw deflate,
+          # like some broken web servers do.
+          Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate body
+        end
+      else
+        body
       end
     end
 
