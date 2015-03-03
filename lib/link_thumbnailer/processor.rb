0d4723d3645bb395da563080ef7298f44ca6fd1a
Anton Mironov <ant.mironov@gmail.com>
2015-03-03 13:50:28 +0300

    parse encoding from response

--- lib/link_thumbnailer/processor.rb
+++ lib/link_thumbnailer/processor.rb
@@ -49,7 +49,7 @@ module LinkThumbnailer
     def perform_request
       response = http.request(url)
       case response
-      when ::Net::HTTPSuccess then decode(response['content-encoding'], response.body)
+      when ::Net::HTTPSuccess then decode(response)
       when ::Net::HTTPRedirection
         call resolve_relative_url(response['location']), redirect_count + 1
       else
@@ -57,8 +57,10 @@ module LinkThumbnailer
       end
     end
 
-    def decode(content_encoding, body)
-      if (!body) || body.empty?
+    def decode(response)
+      content_encoding, body = response['content-encoding'], response.body
+
+      decoded_body = if (!body) || body.empty?
         body
       elsif content_encoding == 'gzip'
         Zlib::GzipReader.new(StringIO.new(body)).read
@@ -73,6 +75,97 @@ module LinkThumbnailer
       else
         body
       end
+
+      encoding = get_encoding_from_headers(response['content-type'])
+      if encoding
+        decoded_body.force_encoding(encoding).encode('UTF-8')
+      else
+        decoded_body
+      end
+    end
+
+    # Return encoding from an HTTP header hash.
+    #
+    # @param headers [Hash]
+    #
+    # @return [String] encoding
+    #
+    def get_encoding_from_headers(type_header)
+      return nil unless type_header
+
+      content_type, params = cgi_parse_header(type_header)
+
+      if params.include?('charset')
+        return params.fetch('charset').gsub(/(\A["']*)|(["']*\z)/, '')
+      end
+
+      # HTTP typically defaults to ISO-8859-1 when not specified.
+      if content_type.include?('text')
+        return 'ISO-8859-1'
+      end
+
+      nil
+    end
+
+    # Parse semi-colon separated, potentially quoted header string iteratively.
+    #
+    # @private
+    #
+    def _cgi_parseparam(s)
+      return enum_for(__method__, s) unless block_given?
+
+      while s[0] == ';'
+        s = s[1..-1]
+        ends = s.index(';')
+        while ends && ends > 0 \
+              && (s[0...ends].count('"') -
+                  s[0...ends].scan('\"').count) % 2 != 0
+          ends = s.index(';', ends + 1)
+        end
+        if ends.nil?
+          ends = s.length
+        end
+        f = s[0...ends]
+        yield f.strip
+        s = s[ends..-1]
+      end
+      nil
+    end
+
+    # Parse a Content-type like header.
+    #
+    # Return the main content-type and a hash of options.
+    #
+    # This method was ported directly from Python's cgi.parse_header(). It
+    # probably doesn't read or perform particularly well in ruby.
+    # https://github.com/python/cpython/blob/3.4/Lib/cgi.py#L301-L331
+    #
+    #
+    # @param [String] line
+    # @return [Array(String, Hash)]
+    #
+    def cgi_parse_header(line)
+      parts = _cgi_parseparam(';' + line)
+      key = parts.next
+      pdict = {}
+
+      begin
+        while p = parts.next
+          i = p.index('=')
+          if i
+            name = p[0...i].strip.downcase
+            value = p[i+1..-1].strip
+            if value.length >= 2 && value[0] == '"' && value[-1] == '"'
+              value = value[1...-1]
+              value = value.gsub('\\\\', '\\').gsub('\\"', '"')
+            end
+            pdict[name] = value
+          end
+        end
+      rescue StopIteration
+      end
+
+      [key, pdict]
     end
 
     def resolve_relative_url(location)
